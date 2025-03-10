611b3f162d77e2aac1133e8d19483291d5151222
OmarAlshandidy <omaralshandiy@gmail.com>
2025-03-10 23:06:52 +0200

    Create Create (Action-View)

--- Company.G04.PL/Controllers/DepartmentController.cs
+++ Company.G04.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.G04.BLL.Interfaces;
 using Company.G04.BLL.Repositries;
+using Company.G04.DAL.Moudel;
+using Company.G04.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G04.PL.Controllers
@@ -18,6 +20,30 @@ namespace Company.G04.PL.Controllers
         {
              var departments = _departmentRepository.GetAll();
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var departments = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+                var count = _departmentRepository.Add(departments);
+                if(count > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ Company.G04.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.G04.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage ="Code Is Required")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name Is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Create At Is Required")]
+        public DateTime CreateAt { get; set; }
+
+    }
+}
