615729f7d1c95603c8b78281fb6ca67a44d6065d
OmarAlshandidy <omaralshandiy@gmail.com>
2025-03-11 17:54:54 +0200

    Edit  (Ation-View)

---
+++ Company.G04.PL/Dtos/UpdateDepartmentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.G04.PL.Dtos
+{
+    public class UpdateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code Is Required")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Name Is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "CreateAt Is Required")]
+        public DateTime CreateAt { get; set; }
+    }
+}
